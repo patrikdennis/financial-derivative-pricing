1dc53a041ca7cb0f17de01d1de84d60b60915640
Patrik Dennis <patrik.franzen.dennis@gmail.com>
2025-03-13 00:53:43 +0100

    Delete: MATLAB version

--- MATLAB/asian/ClosedFormula_AP_geo.m
+++
@@ -1,5 +0,0 @@
-function price = ClosedFormula_AP_geo(s0,sig,r,K,T)
-    dStar = (T/2)*(r-sig.^2/6);
-    d = (log(s0/K) + (T/2)*(r+sig.^2/6)) / (sig*sqrt(T/3));
-    price = -exp(dStar)*s0*normcdf(-d) + K*normcdf(-d+sig*sqrt(T/3));
-end
